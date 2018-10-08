e0da0f15e36949d58aec22d4ae2af991d52202d1
Sup3rc4l1fr4g1l1571c3xp14l1d0c10u5 <fefnir_@hotmail.com>
2018-10-09 07:46:47 +0900

    another

--- JxEditor/kkc/Program.cs
+++ JxEditor/kkc/Program.cs
@@ -15,6 +15,8 @@ namespace KKC1 {
 
             var _LC = args[0];
 
+            KKC2.UnigramTrain("06-pronword.txt", Encoding.UTF8, "06-pron.txt", Encoding.UTF8);
+
             var kkc = new KKC();
             kkc.Load(_LC);
             kkc.Build();
@@ -29,6 +31,188 @@ namespace KKC1 {
 
         }
     }
+    class KKC2 {
+        // https://github.com/tmu-nlp/NLPtutorial2017
+
+        public static void UnigramTrain(string training_file, Encoding enc1, string test_file, Encoding enc2)
+        {
+            //
+            // create unigram
+            //
+            var uni_counts = new Dictionary<string, int>();
+            var uni_total_count = 0;
+
+            foreach (var line in System.IO.File.ReadLines(training_file, enc1))
+            {
+                // split line into an array of words
+                var words = System.Text.RegularExpressions.Regex.Split(line, @"\s+").ToList();
+                // append "</s>" to the end of words
+                //words.Add("</s>");
+                // for each word in words
+                foreach (var word in words)
+                {
+                    uni_counts[word] = uni_counts.ContainsKey(word) ? uni_counts[word] + 1 : 1;
+                    uni_total_count += 1;
+                }
+            }
+
+            var tm = new Dictionary<string, Dictionary<string, double>>();
+            foreach (var kv in uni_counts)
+            {
+                var tmp = kv.Key.Split('_');
+                var yomi = tmp[0];
+                var kaki = (tmp.Length > 1) ? tmp[1] : tmp[0];
+                Dictionary < string, double> pron_words;
+                if (tm.TryGetValue(kaki, out pron_words) == false)
+                {
+                    tm[kaki] = pron_words = new Dictionary<string, double>();
+                }
+                double word_prob;
+                if (pron_words.TryGetValue(yomi, out word_prob) == false)
+                {
+                    word_prob = 0;
+                }
+                word_prob += kv.Value;
+                pron_words[yomi] = word_prob;
+            }
+
+            Console.WriteLine($"tm:");
+            foreach (var kv1 in tm) {
+                var kaki = kv1.Key;
+                foreach (var kv2 in kv1.Value) {
+                    var yomi = kv2.Key;
+                    var pron = kv2.Value / kv1.Value.Count;
+                    Console.WriteLine($"E {kaki} {yomi} {pron:F4}");
+                }
+            }
+
+            //
+            // create bigram
+            //
+
+            // create map counts, context_counts
+            var bi_counts = new Dictionary<string, int>();
+            var bi_context_counts = new Dictionary<string, int>();
+
+            // for each line in the training_file
+            foreach (var line in System.IO.File.ReadLines(training_file, enc1))
+            {
+                // split line into an array of words
+                var words = System.Text.RegularExpressions.Regex.Split(line, @"\s+").Select(x => x.Split('_')[1]).ToList();
+                // append "</s>" to the end and "<s>" to the beginning of words
+                words.Insert(0, "<s>");
+                //words.Add("</s>");
+                // for each i in 1 to length(words)-1
+                for (var i = 1; i <= words.Count - 1; i++)    //注：<s> の後に始まる
+                {
+                    // # 2-gram の分子と分母を加算
+                    var wi1 = words[i - 1];
+                    var wi = words[i];
+                    // counts["w[i-1] w[i]"] += 1   
+                    var key2 = wi1 + " " + wi;
+                    bi_counts[key2] = ((bi_counts.ContainsKey(key2) == false) ? 0 : bi_counts[key2]) + 1;
+                    // context_counts["w[i-1]"] += 1   
+                    bi_context_counts[wi1] = ((bi_context_counts.ContainsKey(wi1) == false) ? 0 : bi_context_counts[wi1]) + 1;
+                    // counts["wi"] += 1   
+                    bi_counts[wi] = ((bi_counts.ContainsKey(wi) == false) ? 0 : bi_counts[wi]) + 1;
+                    // context_counts[""] += 1   
+                    bi_context_counts[""] = ((bi_context_counts.ContainsKey("") == false) ? 0 : bi_context_counts[""]) + 1;
+                }
+            }
+
+            var lm = new Dictionary<string, float>();
+
+            // for each ngram, count in counts
+            foreach (var kv in bi_counts)
+            {
+                var ngram = kv.Key;
+                var count = kv.Value;
+                // split ngram into an array of words # "wi1 wi" => ["wi1", "wi"]
+                var words = System.Text.RegularExpressions.Regex.Split(ngram, @"\s+").ToList();
+                // remove the last element of words
+                words.RemoveAt(words.Count - 1);
+                // join words into context
+                var context = string.Join(" ", words);
+                // probability = counts [ngram] / context_counts[context]
+                var probability = (float)bi_counts[ngram] / bi_context_counts[context];
+                // save
+                lm[ngram] = probability;
+            }
+
+            Console.WriteLine($"lm:");
+            foreach (var kv1 in lm) {
+                Console.WriteLine($"{kv1.Key} {kv1.Value:F4}");
+            }
+
+
+            //
+            // KKC
+            //
+
+            // for each line in file
+            foreach (var line in System.IO.File.ReadLines(test_file, enc2))
+            //for (var line = Console.ReadLine(); line != null ; line = Console.ReadLine())
+            {
+                string answer = null;
+
+                var best_edge  = new List<Tuple<int, int, string>>();
+                var score = new List<Dictionary<string, double>>();
+                // forward step
+                {
+                    best_edge.Clear();
+                    score.Clear();
+                    best_edge.Add(null);
+                    score.Add(new Dictionary<string, double>() { { "<s>", 0 } });
+                    for (var end = 1; end <= line.Length; end++)
+                    {
+                        best_edge.Add(null);
+                        score.Add(new Dictionary<string, double>());
+                        for (var begin = 0; begin <= end - 1; begin++)
+                        {
+                            var pron = line.Substring(begin, end - begin);
+                            var my_tm = (!tm.ContainsKey(pron)) && pron.Length == 1 ? new Dictionary<string, double>() { { pron, 0 } } : tm[pron];
+                            foreach (var kv1 in my_tm)
+                            {
+                                var curr_word = kv1.Key;
+                                var tm_prob = kv1.Value;
+                                foreach (var kv2 in score[begin]) {
+                                    var prev_word = kv2.Key;
+                                    var prev_score = kv2.Value;
+
+                                    var curr_score = prev_score + -Math.Log(tm_prob * lm[prev_word + " " + curr_word]);
+                                    if (curr_score < score[end][curr_word])
+                                    {
+                                        score[end][curr_word] = curr_score;
+                                        best_edge[end] = Tuple.Create(begin, end, curr_word);
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+
+
+                // backward step
+                {
+                    var words = new List<string>();
+                    var next_edge = best_edge[best_edge.Count - 1];
+                    while (next_edge != null)
+                    {
+                        var word = line.Substring(next_edge.Item1, next_edge.Item2 - next_edge.Item1);
+                        words.Add(word);
+                        next_edge = best_edge[next_edge.Item1];
+                    }
+                    answer = String.Join(" ", words.Reverse<string>().ToArray());
+                }
+                // put answer
+                Console.WriteLine(answer);
+            }
+        }
+
+    }
+
+
 
     class KKC {
         // 入力記号集合
